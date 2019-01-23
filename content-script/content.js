c50fe01bfde5d27a92b88a9f36459b25a8b766a5
Mindaugas Jačionis <mindaugasjacionis@82-135-144-188.static.zebra.lt>
2019-01-23 20:46:03 +0200

    Dancing spidey content script based extension

---
+++ content-script/content.js
@@ -0,0 +1,6 @@
+const spidey = document.createElement('img');
+spidey.src = "https://media.giphy.com/media/7isbcNAx367qU/giphy.gif";
+spidey.style.position = 'absolute';
+spidey.style.top = 0;
+spidey.style.right = 0;
+document.querySelector('body').appendChild(spidey);
